b74737a97460f82d832a3a791be080a0c385dcb3
maibrahim <maibrahim@users.noreply.github.com>
2020-01-21 16:29:53 -0500

    Add Users and Change EmailField

--- src/App.js
+++ src/App.js
@@ -8,10 +8,17 @@ const defaultUsers = [
   { id: 3, name: "Frey", email: "f@ag.com", role: "Admin" }
 ];
 
+const emptyUser = {
+  name: "",
+  email: "",
+  role: ""
+};
+
 function App() {
   //Declare state because this info changes over time
   // and we want React to redraw the screen when this data changes.
   const [users, setUsers] = useState(defaultUsers);
+  const [user, setUser] = useState(emptyUser);
 
   //Note that browser passing event //
 
@@ -20,12 +27,32 @@ function App() {
     setUsers(newUsers);
   }
 
+  function onEmailChange(event) {
+    const newUser = { ...user }; // copy user object
+    newUser.email = event.target.value;
+    setUser(newUser);
+  }
+
+  function onChange(event) {
+    // This is the calue just typed in
+    // event.target.value
+
+    //The input someone typed in
+    //event.target.name
+    debugger;
+  }
+
   function renderUser(user) {
     return (
       <tr>
         <td>
           {/* Wrapping deleteUser in arrow to avoid immediate execution */}
-          <button onClick={() => deleteUser(user.id)}>Delete</button>
+          <button
+            aria-label={"Delete " + user.name}
+            onClick={() => deleteUser(user.id)}
+          >
+            Delete
+          </button>
         </td>
         <td>{user.name}</td>
         <td>{user.email}</td>
@@ -37,6 +64,32 @@ function App() {
   return (
     <>
       <h1>Users</h1>
+      <h2>Add User</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input id="name" type="text" onChange={onChange} value={user.name} />
+        </div>
+        <div>
+          <label htmlFor="role">Role</label>
+          <br />
+          <input id="role" type="text" onChange={onChange} value={user.role} />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <br />
+          <input
+            id="email"
+            type="text"
+            onChange={onEmailChange}
+            value={user.email}
+          />
+        </div>
+        <input type="submit" value="Add User" />
+        <br />
+      </form>
+      <div />
       <table>
         <thead>
           <tr>
