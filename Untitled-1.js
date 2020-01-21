611e7cb547f6c02a6bfc9a04e1a216bd30ffc432
maibrahim <maibrahim@users.noreply.github.com>
2020-01-21 14:02:44 -0500

    User Panel
    User Panel - Day 1

---
+++ Untitled-1.js
@@ -0,0 +1,10 @@
+const users = [
+  { id: 1, name: "Cory", email: "c@ag.com", role: "Admin" },
+  { id: 2, name: "Wahid", email: "w@ag.com", role: "User" },
+  { id: 3, name: "Frey", email: "f@ag.com", role: "Admin" }
+];
+
+const cory = users[0];
+const wahid = users[1];
+
+console.log(wahid);
